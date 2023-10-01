532761b9bc8d5197f9136c71169ae8a2f428fb91
Omer Gaiznger <Omerg863@gmail.com>
2023-10-01 22:10:24 +0300

    added sidebar to index html

--- client/index.js
+++ client/index.js
@@ -30,6 +30,15 @@ document.addEventListener("DOMContentLoaded", (event) => {
     } else {
         switchPage(hash.substring(1));
     }
+
+    const body = document.querySelector("body"),
+    sidebar = body.querySelector("nav"),
+    toggle = body.querySelector(".toggle");
+
+
+    toggle.addEventListener("click", () => {
+        sidebar.classList.toggle("close");
+    });
 });
 
 const switchPage = (page) => {
