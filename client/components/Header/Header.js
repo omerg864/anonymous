2e4abb34cc97296025a329b090939ccd2869761c
YarinGombiner <yaringogo@gmail.com>
2023-10-04 12:50:44 +0300

    Update the header css and html files

--- client/components/Header/Header.js
+++
@@ -1,6 +0,0 @@
-function openSidebar() {
-    document.getElementById("mySidebar").style.width = "250px"; // Open the sidebar by setting its width
-}
-function closeSidebar() {
-    document.getElementById("mySidebar").style.width = "0"; // Close the sidebar by setting its width to 0
-}
